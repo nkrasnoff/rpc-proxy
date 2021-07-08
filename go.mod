e15034f194ce0df1f56d041a21a572f9bce7bba9
Nick Krasnoff <krasnoffn@ainfosec.com>
2021-07-08 15:47:46 -0400

    Parse VM specific Rules and store in policy
    Use dbus calls to the Database Daemon(DBD) to access and store vm
    specific rules
    
    Signed-off-by: Nick Krasnoff <krasnoffn@ainfosec.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module rpc-proxy
 
 go 1.15
+
+require github.com/godbus/dbus v4.1.0+incompatible
--- policy/policy.go
+++ policy/policy.go
@@ -3,11 +3,18 @@ package policy
 import (
 	"bufio"
 	"fmt"
+	"github.com/godbus/dbus"
 	"log"
 	"os"
 	"strconv"
 	"strings"
 )
+
+//Rules contains global rules and per domain rules
+type Rules struct {
+	Global []Rule
+	PerVM  map[string][]Rule
+}
 
 //Rule for rpc-proxy Firewall
 type Rule struct {
@@ -55,8 +62,18 @@ const (
 )
 
 //ReadPolicy from config files and return rules
-func ReadPolicy(path string) []Rule {
-	var rules []Rule
+func ReadPolicy(path string) Rules {
+	var rules Rules
+
+	//Read in Global rules from config files
+	rules.Global = readGlobalRules(path)
+	rules.PerVM = readPerVMRules()
+
+	return rules
+}
+
+func readGlobalRules(path string) []Rule {
+	var globalRules []Rule
 
 	file, err := os.Open(path)
 	if err != nil {
@@ -73,14 +90,63 @@ func ReadPolicy(path string) []Rule {
 		}
 
 		r := createRule(strings.Fields(ruleStr))
-		rules = append(rules, r)
+		globalRules = append(globalRules, r)
 		fmt.Println("Rule: ", r)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	return rules
+	return globalRules
+}
+
+func readPerVMRules() map[string][]Rule {
+	perVM := make(map[string][]Rule)
+
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		fmt.Println("Error finding system bus: ", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("I connected to the bus")
+
+	obj := conn.Object("com.citrix.xenclient.db", "/")
+	call := obj.Call("com.citrix.xenclient.db.list", 0, "/vm/")
+	if call.Err != nil {
+		fmt.Println("Error calling db.list: ", call.Err)
+	}
+	var vms []string
+	call.Store(&vms)
+
+	for _, uuid := range vms {
+		call = obj.Call("com.citrix.xenclient.db.list", 0, "/vm/"+uuid+"/rpc-firewall-rules")
+		if call.Err != nil {
+			fmt.Println("Error calling db.list: ", call.Err)
+		}
+		var ruleIds []string
+		call.Store(&ruleIds)
+
+		var vmRules []Rule
+		for _, id := range ruleIds {
+			//fmt.Println("/vm/" + uuid + "/rpc-firewall-rules/" + id)
+			call = obj.Call("com.citrix.xenclient.db.read", 0, "/vm/"+uuid+"/rpc-firewall-rules/"+id)
+			if call.Err != nil {
+				fmt.Println("Error calling db.read: ", call.Err)
+			}
+			var ruleStr string
+			call.Store(&ruleStr)
+			//fmt.Println(ruleStr)
+
+			r := createRule(strings.Fields(ruleStr))
+			vmRules = append(vmRules, r)
+
+		}
+		perVM[uuid] = vmRules
+	}
+
+	return perVM
 }
 
 func createRule(ruleSlc []string) Rule {
